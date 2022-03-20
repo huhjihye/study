d145ef85a18d55e66fad1b3ccbdcef9c9489e115
floramiss <floramiss@naver.com>
2022-03-21 00:58:46 +0900

    study

---
+++ src/main/java/alone2.java
@@ -0,0 +1,14 @@
+public class alone2 {
+    public static void main(String[] args) {
+        //문자열(String) 변수 name 만들기
+        String name;
+
+        //변수 ㅜ믇에 특정 값 저장하기
+        name="jihye";
+
+                //결과 출력
+        System.out.println("안녕하세요! 반갑습니다! 제 이름은, ");
+        System.out.println(name);
+        System.out.println("입니다. 잘 부탁드립니다!~");
+    }
+}
