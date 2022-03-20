c6bc5a3d03335552e005283b6b1661589d1c2333
floramiss <floramiss@naver.com>
2022-03-20 20:29:09 +0900

    study

---
+++ src/main/java/alone.java
@@ -0,0 +1,10 @@
+public class alone {
+    public static void main(String[] args) {
+        double a = 10.3;
+        double b = 9.6;
+        double c= 10.1;
+
+        System.out.println((a + b+ c)/3);
+    }
+
+}
