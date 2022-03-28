746a84f0aaaf94c104155b8eab48eeef097eae64
floramiss <floramiss@naver.com>
2022-03-28 13:06:42 +0900

    study

---
+++ src/main/java/doitjava01_01.java
@@ -0,0 +1,15 @@
+public class doitjava01_01 {
+    public static void main(String[] args) {
+
+        int num=10;
+        int bNum=0B1010;
+        int oNum=012;
+        int hNum=0xa;
+
+        System.out.println(num);
+        System.out.println(bNum);
+        System.out.println(oNum);
+        System.out.println(hNum);
+
+    }
+}
