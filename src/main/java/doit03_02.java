5622396170e51bf1fe25c8592cd877cad73f35b4
floramiss <floramiss@naver.com>
2022-03-31 22:51:46 +0900

    2022.03.31 do it java -변수와 자료형(3)

---
+++ src/main/java/doit03_02.java
@@ -0,0 +1,16 @@
+public class doit03_02 {
+    public static void main(String[] args) {
+        byte bNum =10;
+        int num =bNum;
+
+        System.out.println(num);
+
+        long INum =10;
+        float fNum =INum;
+
+        System.out.println(fNum);
+
+        double dNum = fNum +num;
+        System.out.println(dNum);
+    }
+}
