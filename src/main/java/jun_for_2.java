35748851ff31da35bc3a949c6417e3e8118e0d99
floramiss <floramiss@naver.com>
2022-04-05 15:16:36 +0900

    2022.04.05 백준 -반복문(1)

---
+++ src/main/java/jun_for_2.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class jun_for_2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int c = s.nextInt();
+
+        for (int i = 1; i <= c; i++) {
+            int a = s.nextInt();
+            int b = s.nextInt();
+            System.out.println(a + b);
+        }
+
+    }
+
+}
