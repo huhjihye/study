f0dbe02be8b450c5a6b4bc3efbbb1fd3b659f3f6
floramiss <floramiss@naver.com>
2022-06-04 22:48:50 +0900

    2022.06.04

--- src/main/java/study/star6_1.java
+++ src/main/java/study/star6_1.java
@@ -8,8 +8,8 @@ public class star6_1 {
         int i, j;
         int a = sc.nextInt();
 
-        int st = a / 2 ;
-        int end = a / 2 + 1;
+        int st = a/2 ;
+        int end = a/2+1;
 
         for (i = 1; i <=a; i++) {
             for (j = 1; j <=end; j++) {
