ee2748db818aab5cdb53dfd09ba1d6eec919a793
floramiss <floramiss@naver.com>
2022-06-13 19:09:02 +0900

    2022.06.13 백준 15596

---
+++ src/main/java/JUN/jun_15596.java
@@ -0,0 +1,17 @@
+package JUN;
+
+
+public class jun_15596 {
+
+
+    long sum(int[] a) {
+        long sum = 0; //a 배열 정수 합
+
+            for (int i = 0; i < a.length; i++) {
+                sum += a[i];
+            }
+            return sum;
+
+        }
+    }
+
