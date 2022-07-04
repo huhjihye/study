a08e5d64259aae2de9d3d783060198172476670e
floramiss <floramiss@naver.com>
2022-07-05 00:27:55 +0900

    2022.07.05 백준 문자열 집합

---
+++ src/main/java/JUN/jun_14425.java
@@ -0,0 +1,32 @@
+package JUN;
+import java.io.*;
+import java.util.HashMap;
+import java.util.StringTokenizer;
+
+public class jun_14425{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        HashMap<String, Integer> map = new HashMap<>();
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            map.put(br.readLine(), 1);
+        }
+        for (int i = 0; i < M; i++) {
+            String str = br.readLine();
+            if (map.get(str) != null) {
+                cnt++;
+            }
+        }
+        bw.write(cnt + "\n");
+
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
